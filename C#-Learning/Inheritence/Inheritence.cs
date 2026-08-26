dfffa667f1946340d4dc63311cde68ef5a6d8ef4
Hanzala-Imdad <hanzalaimdad70@gmail.com>
2026-08-26 13:38:00 +0500

    Multilevel Inheritence Practice

--- C#-Learning/Inheritence/Inheritence.cs
+++ C#-Learning/Inheritence/Inheritence.cs
@@ -43,6 +43,24 @@ class Program
         Dog d = new Dog();
         d.Eat();   // Parent Animal se inherited
         d.Bark();  // Dog ka apna method
+
+        Developer dev = new Developer();
+        dev.Introduce();
+        dev.work();
+        dev.code();
+
+        dog dd = new dog();
+        dd.eat();
+        dd.bark();
+        dd.Walk();
+
+        universityStuent US = new universityStuent();
+        US.name = "Hanzala";
+        US.university = "Preston";       
+ 
+        US.Introducee();
+        US.study();
+        US.showuniversity();
     }
 }
 
@@ -84,5 +102,77 @@ class Dog : Animal
     public void Bark()
     {
         Console.WriteLine($"{Name} says Woof!");
+    }
+}
+             //-------MULTILEVEL INHERITENCE---------//
+
+// Question 1 of Multilevel Inheritence
+
+class person
+{
+    public void Introduce()
+    {
+        Console.WriteLine($"I'm a Person");
+    }
+
+}
+class Employee : person
+{
+    public void work()
+    {
+        Console.WriteLine($"Employee is Working ");
+    }
+}
+class Developer : Employee
+{
+    public void code()
+    {
+        Console.WriteLine("Developer is Coding");
+    }
+}
+ 
+// Question 2 of Multilevel Inheritence "Animal Chain"
+class Animall
+{
+    public void eat()
+    {
+        Console.WriteLine("Animal is Eating");
+    }
+}
+class Mammel : Animall
+{
+    public void Walk()
+    {
+        Console.WriteLine("Mammel is Walking");
+    }
+}
+class dog : Mammel
+{
+    public void bark()
+    {
+        Console.WriteLine("Dog is Barking");
+    }
+}
+class personn
+{
+   public string name;
+    public void Introducee()
+    {
+        Console.WriteLine($"I'm {name}");
+    }
+}
+class student : personn
+{
+    public void study()
+    {
+        Console.WriteLine("student is studing");
+    }
+}
+class universityStuent : student
+{
+    public string university;
+   public void showuniversity()
+    {
+       Console.WriteLine($"Name of University is {university}");
     }
 }
