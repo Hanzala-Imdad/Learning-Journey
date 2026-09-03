2f9cb1de09ff73ef8901f1ad94111d266ba39b72
Hanzala-Imdad <hanzalaimdad70@gmail.com>
2026-09-03 16:12:29 +0500

    Enums Practice

---
+++ C#-Learning/Enums/Enums.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=========Question 1 of Enums=========");
+        Days today = Days.Monday;
+        Console.WriteLine($"{today} is the day of the week.");
+
+        Console.WriteLine("\n=========Question 2 of Enums=========");
+        colour favoriteColor = colour.Blue;
+        Console.WriteLine($"My favorite color is {favoriteColor}.");
+
+        Console.WriteLine("\n=========Question 3 of Enums=========");
+        status Status = status.Pending;
+        if (Status == status.Approved)
+        {
+            Console.WriteLine($"The status of the request is {Status}.");
+        }
+        else if (Status == status.Pending)
+        {
+            Console.WriteLine($"The status of the request is {Status}.");
+        }
+        else
+        {
+            Console.WriteLine($"The status of the request is {Status}.");
+        }
+    }
+}
+
+        //===============ENUMS==============//
+//Question 1 of Enums
+enum Days
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+//Question 2 of Enums
+enum colour
+{
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Orange,
+    Purple
+}
+
+//Question 3 of Enums
+enum status
+{
+    Pending,
+    Approved,
+    Rejected,
+}
